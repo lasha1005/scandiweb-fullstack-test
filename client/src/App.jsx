b6a72f036b21af870f2aefb481d65c916f0bfbbe
lasha1005 <124585845+lasha1005@users.noreply.github.com>
2025-07-01 17:19:14 +0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider, Navigate } from 'react-router-dom'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 import Products from './Pages/Products'
 import Layout from './components/Layout'
 import ProductDetails from './Pages/ProductDetails'
@@ -9,7 +9,7 @@ import { CartProvider } from './context/CartContext'
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<Layout />}>
-    <Route index element={<Navigate to="/all" replace />} />
+    <Route index element={<Products categoryName="all" />} />
     <Route path='all' element={<Products categoryName="all"/>}/>
     <Route path='tech' element={<Products categoryName="tech"/>}/>
     <Route path='clothes' element={<Products categoryName="clothes"/>}/>
