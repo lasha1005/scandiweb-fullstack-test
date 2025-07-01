4f4bd89bb0c4cbd8ad91362a28bcc3877fcdb58e
lasha1005 <124585845+lasha1005@users.noreply.github.com>
2025-07-01 15:56:35 +0400

    Update HeaderLink.jsx

--- client/src/components/HeaderLink.jsx
+++ client/src/components/HeaderLink.jsx
@@ -3,10 +3,7 @@ import { NavLink, useLocation } from 'react-router'
 function HeaderLink({path, children}) {
   const location = useLocation();
   const isActive = location.pathname === path;
-<<<<<<< HEAD
   
-=======
->>>>>>> 1b9c6610d93571d318f9f118fd36874f4f99b291
   return (
     <NavLink 
         to={path}
