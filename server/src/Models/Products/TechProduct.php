2a683ee7da488e4725fdada8005f2d43b14955b1
lasha1005 <124585845+lasha1005@users.noreply.github.com>
2025-07-03 15:38:48 +0400

    Update TechProduct.php

--- server/src/Models/Products/TechProduct.php
+++ server/src/Models/Products/TechProduct.php
@@ -6,6 +6,8 @@ namespace App\Models\Products;
 
 class TechProduct extends Products
 {
+    use ProductArrayTrait;
+    
     public function __construct(array $data)
     {
         parent::__construct($data);
@@ -14,19 +16,5 @@ class TechProduct extends Products
     public function getType(): string 
     {
         return "tech";
-    }
-
-    public function toArray(): array
-    {
-         return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'brand' => $this->brand,
-            'description' => $this->description,
-            'in_stock' => $this->in_stock,
-            'category' => $this->getType(),
-            'gallery' => $this->gallery,
-            'price' => $this->price
-        ];
     } 
 }
