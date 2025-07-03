8e8304ea33ce695db4ff47719aa050908b9c3616
lasha1005 <124585845+lasha1005@users.noreply.github.com>
2025-07-03 15:38:24 +0400

    Update ClothesProduct.php

--- server/src/Models/Products/ClothesProduct.php
+++ server/src/Models/Products/ClothesProduct.php
@@ -6,6 +6,8 @@ namespace App\Models\Products;
 
 class ClothesProduct extends Products 
 {
+    use ProductArrayTrait;
+    
     public function __construct(array $data)
     {
         parent::__construct($data);
@@ -14,19 +16,5 @@ class ClothesProduct extends Products
     public function getType(): string
     {
         return "clothes";
-    }
-
-    public function toArray(): array 
-    {
-         return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'brand' => $this->brand,
-            'description' => $this->description,
-            'in_stock' => $this->in_stock,
-            'category' => $this->getType(),
-            'gallery' => $this->gallery,
-            'price' => $this->price
-        ];
     }
 }
